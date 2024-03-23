d8da597bb64f77d85d2b52f986cd2370a42f8bc3
Michel Romero <studentenherz@gmail.com>
2024-03-23 01:28:41 -0300

    Add utils for input validation

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+pub mod utils;
+
 #[derive(Deserialize, Serialize)]
 pub struct LoginResponse {
     pub token: String,
---
+++ common/src/utils.rs
@@ -0,0 +1,22 @@
+use regex::Regex;
+
+pub fn is_valid_email(email: &str) -> bool {
+    let email_regex = Regex::new(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$").unwrap();
+    email_regex.is_match(email)
+}
+
+pub fn normalize_email(email: &str) -> String {
+    email.trim().to_lowercase()
+}
+
+pub fn is_valid_username(username: &str) -> bool {
+    let username_regex = Regex::new(r"^[a-zA-Z0-9_]+$").unwrap();
+    username.len() >= 3 && username.len() <= 100 && username_regex.is_match(username)
+}
+
+pub fn is_valid_password(password: &str) -> bool {
+    let allowed_chars_regex =
+        Regex::new(r#"^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?~ ]+$"#).unwrap();
+
+    allowed_chars_regex.is_match(password) && password.len() >= 3 && password.len() <= 100
+}
