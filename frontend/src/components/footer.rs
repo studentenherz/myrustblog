23e33c399c6528eb2ade62fcbc8acccc145fa219
Michel Romero <studentenherz@gmail.com>
2024-04-21 10:14:14 -0300

    Update the footer

--- frontend/src/components/footer.rs
+++ frontend/src/components/footer.rs
@@ -4,7 +4,16 @@ use yew::prelude::*;
 pub fn footer() -> Html {
     html! {
         <footer class="center-content">
-            { "by " } <a href={"https://github.com/studentenherz"} target={"_blank"}> {"Michel Romero Rodríguez"} </a>
+            <div class="separator" ></div>
+            <div class="socials">
+                <a href="https://github.com/studentenherz" target={"_blank"}><i class="icon fa-brands fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/studentenherz/" target={"_blank"}><i class="icon fa-brands fa-linkedin"></i></a>
+                <a href="https://t.me/michelromero" target={"_blank"}><i class="icon fa-brands fa-telegram"></i></a>
+            </div>
+            <center>
+                { "Made using " } <a href={"https://yew.rs/"} target={"_blank"}> {"Yew"} </a>
+                <i>{ " by Michel Romero"}</i>
+            </center>
         </footer>
     }
 }
