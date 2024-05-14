cd20618edfa817950e55d23fc9b17f24123de694
Michel Romero <studentenherz@gmail.com>
2024-05-13 22:39:23 -0300

    Add revision hash and link

--- frontend/build.rs
+++ frontend/build.rs
@@ -1,4 +1,5 @@
 use dotenv::dotenv;
+use std::process::Command;
 
 fn main() {
     // println!("cargo:rerun-if-changed=.env");
@@ -9,4 +10,28 @@ fn main() {
             println!("cargo:rustc-env={}={}", key, value);
         }
     }
+
+    // Set commit hash and url
+    let commit_hash_output = Command::new("git")
+        .args(["describe", "--dirty=*", "--always", "--match", "NOT A TAG"])
+        .output()
+        .expect("Failed to get commit hash");
+    let commit_hash =
+        String::from_utf8(commit_hash_output.stdout).expect("Invalid UTF-8 in commit hash");
+    let commit_hash = commit_hash.trim(); // Remove any extra whitespace
+
+    let repo_url_output = Command::new("git")
+        .args(["remote", "get-url", "origin"])
+        .output()
+        .expect("Failed to get remote URL");
+    let repo_url = String::from_utf8(repo_url_output.stdout).expect("Invalid UTF-8 in repo URL");
+    let repo_url = repo_url.trim(); // Remove any extra whitespace
+
+    let base_url = repo_url
+        .replace("git@github.com:", "https://github.com/")
+        .replace(".git", "");
+    let commit_url = format!("{}/tree/{}", base_url, commit_hash);
+
+    println!("cargo:rustc-env=APP_COMMIT_HASH={}", commit_hash);
+    println!("cargo:rustc-env=APP_COMMIT_URL={}", commit_url);
 }
--- frontend/src/components/footer.rs
+++ frontend/src/components/footer.rs
@@ -5,16 +5,17 @@ pub fn footer() -> Html {
     html! {
         <footer class="center-content">
             <div class="separator" ></div>
-            <div class="socials">
+            <section class="socials">
                 <a href="https://github.com/studentenherz" target={"_blank"}><i class="icon fa-brands fa-github"></i></a>
                 <a href="https://www.linkedin.com/in/studentenherz/" target={"_blank"}><i class="icon fa-brands fa-linkedin"></i></a>
                 <a href="https://t.me/michelromero" target={"_blank"}><i class="icon fa-brands fa-telegram"></i></a>
                 <a href="/rss" target={"_blank"}><i class="icon fa-solid fa-square-rss"></i></a>
-            </div>
+            </section>
             <center>
                 { "Made using " } <a href={"https://yew.rs/"} target={"_blank"}> {"Yew"} </a>
                 {" and "} <a href={"https://actix.rs/"} target={"_blank"}> {"Actix"} </a>
-                <i>{ " by Michel Romero"}</i>
+                <i>{ " by Michel Romero "}</i>
+                <a href={env!("APP_COMMIT_URL")} target={"_blank"}> {env!("APP_COMMIT_HASH")} </a>
             </center>
         </footer>
     }
