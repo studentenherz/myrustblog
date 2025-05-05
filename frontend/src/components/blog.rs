a7912ea670e826656c3e28c54db637ffb0c56382
Michel Romero <studentenherz@gmail.com>
2025-05-05 02:45:05 -0300

    Add header with avatar to blog list page

--- frontend/src/components/blog.rs
+++ frontend/src/components/blog.rs
@@ -29,34 +29,42 @@ pub fn blog(BlogProps { page, pages, posts }: &BlogProps) -> Html {
     };
 
     html! {
-        <div class="blog-posts-list">
-            <div class="posts-container">
-                { for posts.iter().map(|post| html! {
-                    <PostCard post={post.clone()} />
-                }) }
+        <>
+            <section class="blog-header">
+                <img class="avatar-img" src="static/avatar.webp" alt="d^_^b" />
+                <p>{ "...about physics, mathematics, programmig, hacking, and anything I find ineresting enough to share." }</p>
+            </section>
+
+
+            <div class="blog-posts-list">
+                <div class="posts-container">
+                    { for posts.iter().map(|post| html! {
+                        <PostCard post={post.clone()} />
+                    }) }
+                </div>
+                <div class="posts-container-navigation">
+                    if let Some(prev_url) = prev_page_url {
+                        <a href={ prev_url }>
+                            <i class="icon-left icon"></i> { "Previous page" }
+                        </a>
+                    } else {
+                        <span class="disabled">
+                            <i class="icon-left icon"></i> { "Previous page" }
+                        </span>
+                    }
+                    <div>{ page } { " / " }  {pages} </div>
+                    if let Some(next_url) = next_page_url {
+                        <a href={ next_url }>
+                            { "Next page" } <i class="icon-right icon"></i>
+                        </a>
+                    }
+                    else {
+                        <span class="disabled">
+                            { "Next page" } <i class="icon-right icon"></i>
+                        </span>
+                    }
+                </div>
             </div>
-            <div class="posts-container-navigation">
-                if let Some(prev_url) = prev_page_url {
-                    <a href={ prev_url }>
-                        <i class="icon-left icon"></i> { "Previous page" }
-                    </a>
-                } else {
-                    <span class="disabled">
-                        <i class="icon-left icon"></i> { "Previous page" }
-                    </span>
-                }
-                <div>{ page } { " / " }  {pages} </div>
-                if let Some(next_url) = next_page_url {
-                    <a href={ next_url }>
-                        { "Next page" } <i class="icon-right icon"></i>
-                    </a>
-                }
-                else {
-                    <span class="disabled">
-                        { "Next page" } <i class="icon-right icon"></i>
-                    </span>
-                }
-            </div>
-        </div>
+        </>
     }
 }
